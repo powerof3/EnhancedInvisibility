412e31782973a1efec3dab172c028d7caacf8d13
powerof3 <32599957+powerof3@users.noreply.github.com>
2022-01-15 10:18:25 +0530

    reduce trampoline space

--- src/main.cpp
+++ src/main.cpp
@@ -331,7 +331,7 @@ extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_s
 	SKSE::Init(a_skse);
 
 	if (Settings::GetSingleton()->LoadSettings()) {
-		SKSE::AllocTrampoline(42);
+		SKSE::AllocTrampoline(14);
 
 		Refraction::Install();
 		MakeInvisible::Install();
